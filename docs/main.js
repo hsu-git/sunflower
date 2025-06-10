226646b8220627de5a39762fbafa72f178491896
hsu-git <hsu080206@gmail.com>
2025-06-10 12:46:06 +0900

    폼처리

---
+++ docs/main.js
@@ -0,0 +1,10 @@
+const apiKey = document.querySelector("#prompt-key");
+const promptInput = document.querySelector("#prompt-input");
+
+const form = document.querySelector("#prompt-form");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  alert(
+    `프롬프트 전송! API 키: ${apiKey.value}, 프롬프트: ${promptInput.value}`
+  );
+});
